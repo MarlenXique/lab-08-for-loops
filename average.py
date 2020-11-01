a858b59fc74bc50a0616f667965b88185b7fada2
Marlen Xique <marlenxique@Marlens-MBP.lan>
2020-11-01 00:00:06 -0400

    message

---
+++ average.py
@@ -0,0 +1,5 @@
+total = 0
+for i in range(0, 4, 1):
+    number = int(input("Enter number"))
+    total = total + number
+print("The average is =", total/4)
---
+++ largest.py
@@ -0,0 +1,6 @@
+largest = 0;
+for i in range(0, 4, 1):
+    number = int(input("please enter a number"))
+    if (number >= largest):
+        largest = number
+print("The largest number:", largest)
