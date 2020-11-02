1552375d2dc6e4a4552cc89a5588fc7a8ecb0ba9
Marlen Xique <marlenxique@Marlens-MBP.lan>
2020-11-02 15:48:24 -0500

    During class

--- dictionary.py
+++ dictionary.py
@@ -1,8 +1,11 @@
 myData = {"effective top tube length": 515, "seat tube length": 500, "seat tube angle": 74.4, "head tube angle": 70.5, "stack": 513, "reach": 367, "standover height": 755}
+myKeys = []
+myValues = []
+
 for k, v in myData.items():
-  print('key:', k, ', value:', v)
-y = (myData)
-limit = 100
-for x in range(1, limit + 1):
-    y.append(x)
-print(y)
+    print("key:" + str(k) + ", value:" + str(v))
+    myKeys.append(k)
+    myValues.append(v)
+
+print("All Keys", str(myKeys))
+print("All Values", str(myValues))
